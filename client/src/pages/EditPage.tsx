bf286352d95a2f730463df21189ca401afb127fa
Andy Tseng <anby1611@gmail.com>
2023-01-22 23:21:33 -0800

    Edit page save logic

--- client/src/pages/EditPage.tsx
+++ client/src/pages/EditPage.tsx
@@ -1,17 +1,26 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import axios, { AxiosResponse } from 'axios'
 import { Container } from 'react-bootstrap'
 import { useNavigate, useParams } from 'react-router-dom'
 import NoteForm, { NoteMutateType } from '../components/NoteForm'
 
 const EditPage = () => {
-	const { categoryId, noteId } = useParams()
+	const { noteId } = useParams()
 	const navigate = useNavigate()
+	const queryClient = useQueryClient()
+	const { data, isLoading, isError } = useQuery({
+		queryKey: ['notes', noteId],
+		queryFn: async () => {
+			const { data } = await axios.get(`/api/notes/${noteId}`)
+			return data
+		},
+	})
 	const mutation = useMutation({
 		mutationFn: (noteUpdate: NoteMutateType): Promise<AxiosResponse> => {
 			return axios.put(`/api/notes/${noteId}`, noteUpdate)
 		},
-		onSuccess: () => {
+		onSuccess: ({ data }) => {
+			queryClient.setQueriesData(['notes', noteId], data)
 			navigate('..')
 		},
 	})
@@ -19,7 +28,18 @@ const EditPage = () => {
 		<>
 			<Container className="my-5 px-4 ">
 				<h1 className="mb-4">Edit Note</h1>
-				<NoteForm mutation={mutation} />
+				{isLoading ? (
+					<h3>Loading...</h3>
+				) : isError ? (
+					<h3>Something Wrong...</h3>
+				) : (
+					<NoteForm
+						mutation={mutation}
+						title={data.title}
+						markdown={data.body}
+						tags={data.tags}
+					/>
+				)}
 			</Container>
 		</>
 	)
